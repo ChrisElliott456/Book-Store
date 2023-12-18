ea7324f707d631d20d82b3d21c0ae168ebebbc43
ChrisElliott <chrisae456@gmail.com>
2023-12-18 14:17:05 -0500

    add implementation of interface

---
+++ src/main/java/BookDaoImpl.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+public class BookDaoImpl implements BookDao{
+
+    @Override
+    public void addBook(Book book) {
+
+    }
+
+    @Override
+    public void updateBook(Book book) {
+
+    }
+
+    @Override
+    public void deleteBook(int id) {
+
+    }
+
+    @Override
+    public Book getBookById(int bookId) {
+        return null;
+    }
+
+    @Override
+    public List<Book> getAllBooks() {
+        return null;
+    }
+}
---
+++ src/main/java/BookStoreApp.java
@@ -0,0 +1,8 @@
+import java.sql.Connection;
+
+public class BookStoreApp {
+    public static void main(String[] args) {
+        JDBConnection.getConnection();
+
+    }
+}
